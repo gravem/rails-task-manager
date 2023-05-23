93d4b9e99d89181cae5d62c17af77b47d186ed29
Aasmund Gravem <aasmund.gravem@gmail.com>
2023-05-23 01:17:39 +0100

    Creating a bunch of files that connects the task app. Just added the edit task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks' to: 'tasks#tasks'
+  get '/tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
